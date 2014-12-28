8b7542eb3da1b9dd4c3f846b8790d03ee93e130d
kumok <bhh@byu.edu>
2014-12-28 16:12:10 -0700

    Final

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -18,12 +18,45 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+   
+    NSString *myName = @"Brian";
+   /* NSString *introduction = [NSString stringWithFormat:@"My name is %@", myName];
+    NSString *japaneseIntro = [myName stringByAppendingString:@" to moushimasu"];
     
-    
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntro); */
+
+    countDownTillIntroduction(4);
+    printIntroductions(myName);
     return YES;
 }
 
 // Define new functions here...
+int printIntroductions(NSString *name)
+{
+ 
+    NSString *introduction = [NSString stringWithFormat:@"My name is %@", name];
+    NSString *japaneseIntro = [name stringByAppendingString:@" to moushimasu"];
+    
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntro);
+
+
+    return 0;
+}
+
+int countDownTillIntroduction(int numberOfDays)
+{
+    if (numberOfDays == 0) {
+        NSLog(@"The time has come");
+    }
+    else {
+        NSLog(@"%d days left until introductions", numberOfDays);
+        int oneLessday = numberOfDays - 1;
+        countDownTillIntroduction(oneLessday);
+    }
+    return 0;
+}
 
 
 
